4a406dd30b05e6d2ae3e2f518947559741c1346c
frenkypicasso <frenky.picasso@seznam.cz>
2016-12-02 02:21:13 +0100

    Mullti and single Mains. Work for multi data sets anf single dataset as well.

--- Features_evaluating/Test.py
+++ Features_evaluating/Test.py
@@ -1,38 +1,36 @@
 
+class Base(object):
+    def __init__(self):
+        self.name = None
+        self.value = None
 
-temp = dict.fromkeys(["a", "b", "c"], True)
+    def get_name(self):
+        return self.name
+
+    def smrdis(self):
+        print "je to: ", self.name
+
+    def get_value(self):
+        print self.value
 
 
-print temp
+class Derivate(Base):
+    def __init__(self, n1, n2, value):
+        super(Derivate, self).__init__()
+        self.n1 = n1
+        self.n2 = n2
+        self.value = value
+
+    def set_name(self):
+        self.name = "ahoj svete"
+
+    def get_name(self):
+        return self.name
 
 
-
-# class Base(object):
-#     def __init__(self):
-#         self.name = None
-#
-#     def get_name(self):
-#         return self.name
-#
-#     def smrdis(self):
-#         print "je to: ", self.name
-#
-#
-# class Derivate(Base):
-#     def __init__(self, n1, n2):
-#         super(Derivate, self).__init__()
-#         self.n1 = n1
-#         self.n2 = n2
-#
-#     def set_name(self):
-#         self.name = "ahoj svete"
-#
-#     def get_name(self):
-#         return self.name
-#
-#
-# if __name__ == "__main__":
-#     temp = Derivate(1,2)
-#     temp.set_name()
-#     print temp.get_name()
-#     temp.smrdis()
+if __name__ == "__main__":
+    temp = Derivate(1,2,5)
+    temp.set_name()
+    print temp.get_name()
+    temp.smrdis()
+    temp.get_value()
